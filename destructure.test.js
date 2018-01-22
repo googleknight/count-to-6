95cb218be898cb30419a46dc760e96ef86e14956
Shubham Mathur <shubham.mathur@engineer.com>
2018-01-22 13:54:00 +0530

    Destructure test cases added

--- destructure.test.js
+++ destructure.test.js
@@ -5,3 +5,15 @@ test('Objects must be equal', () => {
     const expectation = { username: 'joe', email: 'joe@mail.com' };
     expect(arrayToObject(inputArray)).toEqual(expectation);
 });
+
+test('Objects must handle empty objects', () => {
+    const inputArray = ['','','','','' ];
+    const expectation = { username: '', email: '' };
+    expect(arrayToObject(inputArray)).toEqual(expectation);
+});
+
+test('Return type must be object', () => {
+    const inputArray = ['1', 'joe', 'joe@mail.com', 'jon', 'asd'];
+    const expectation = 'object';
+    expect(typeof arrayToObject(inputArray)).toEqual(expectation);
+});
